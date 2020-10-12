c28e56590064bd28700591e2a5de393bc6118593
Alexander Ma <alexanderzma@hotmail.com>
2020-10-12 16:28:04 -0700

    Added dev tools

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.on('ready', async () => {
 	Menu.setApplicationMenu(null)
 	const win = new BrowserWindow({})
 	win.loadFile(path.resolve(__dirname, 'page.html'))
+	if (!app.isPackaged) {
+		const { default: install } = require('electron-devtools-installer')
+		win.webContents.openDevTools()
+	}
 })
