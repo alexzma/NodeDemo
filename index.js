a5a957d2ff4c84bfbd3a51200bdd8bf9385d31e9
Alexander Ma <alexanderzma@hotmail.com>
2020-10-12 16:21:55 -0700

    Make basic window

---
+++ index.js
@@ -0,0 +1,8 @@
+const { app, BrowserWindow, Menu, dialog } = require('electron')
+const path = require('path')
+
+app.on('ready', async () => {
+	Menu.setApplicationMenu(null)
+	const win = new BrowserWindow({})
+	win.loadFile(path.resolve(__dirname, 'page.html'))
+})
